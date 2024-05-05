7715c26751def6e6b5c6705fc324cfe395710132
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-05-04 22:36:47 -0500

    cree mi archivo operador_morza

---
+++ lecture.py
@@ -0,0 +1,24 @@
+# EJEMPLOS DE MORZA.
+# determinaremos si es falso o verdad de variable feliz.
+
+feliz =True
+print(feliz)
+
+
+#2.EJEMPLO MORZA.
+print(feliz :=True)
+
+
+#3.ejemplo.ete programa te permite guardar y salir completamente con morza.
+
+#comidas =[]
+#while True:
+#     comida=input("¿que comida te gusta?")
+#      if comida=="salir":
+#          break
+#     comidas.append(comida)
+
+comidas =[]
+while (comida:= input("¿que comida te gusta ¿")) != "salir":
+    comidas.append(comida)
+
